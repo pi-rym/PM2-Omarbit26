8005744a9a21af7873190a10c3dcf6c5c901b469
Omarbit26 <omaraliaga26bit@gmail.com>
2024-03-12 19:18:21 -0500

    Homework n°6 se agrego axios al package json y se realizo request usando axios y asincronismo

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,16 +1,34 @@
 const {Repository} = require("./Repository")
 const {addCardshtml} = require("./renderCards")
 const {Movie} = require("./Movie")
+const axios = require('axios')
 
 const repository =  new Repository();
 
-const showData = (data)=>{
-    console.log(data)
-    data.forEach(m=>repository.createMovie(m))
-    console.log(repository);
-    addCardshtml(repository);
+/*
+const renderMovies = async () =>{
+    try{
+        const promisePeliculas = await axios.get('https://students-api.up.railway.app/movies');
+        const data_movie = promisePeliculas.data
+        data_movie.forEach(m=>repository.createMovie(m));
+        addCardshtml(repository);
+        
+    }
+    catch(error){
+        console.log("no se puedo encontrar la informacion de peliculas. Refresque la página.")
+    }
 }
+renderMovies();*/
+
+const promiseMovie = axios.get('https://students-api.up.railway.app/movies');
+
+promiseMovie.then((response)=>{const data_movie = response.data;
+    data_movie.forEach(m=>repository.createMovie(m));
+    addCardshtml(repository);
+})
+.catch((error) => { alert("fail movies");console.log(error)});
 
 
-$.get('https://students-api.up.railway.app/movies',showData)
+
+
 
